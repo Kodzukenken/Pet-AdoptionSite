d894df96efb3a2b8cc2f9e6a0812a93563882e2f
Kodzukenken <abigail.01k4w@gmail.com>
2024-11-06 13:37:33 +0700

    editing footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -2,9 +2,26 @@ const logo = "/images/logo.png";
 
 export default function Footer() {
     return(
-        <div className="">
-
-
-        </div>
+        <div className="bg-primary-3 h-fit w-full pt-8 sm:py-4 px-8 flex flex-col items-center text-primary-1 font-bold">
+        <div className="w-full flex flex-col-reverse sm:flex-row items-center justify-between">
+          <img src={logo} alt="Logo" className="w-32" />
+          <div className="flex flex-col gap-y-2 items-end lg:flex-row lg:gap-x-4 text-xs md:text-sm lg:text-base">
+            <div className="flex flex-row items-center lg:gap-2">
+              <div className="text-right">
+                
+              </div>
+             
+            </div>
+            <div className="flex flex-row lg:gap-2 items-center text-right hover:text-white">
+              <p>StudentsxCEOs International Summit</p>
+            </div>
+            <div className="flex flex-row lg:gap-2 items-center text-right hover:text-white">
+              <p className="">@intersummitsxc</p>
+            </div>
+            
+  
+          </div>
+          </div>
+          </div>
     );
 }
