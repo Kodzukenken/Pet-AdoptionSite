e1825f39e5ee0f96fc7e7a0a9c3f618010cd592b
michellelyandryy <michelle.lyandry@student.sgu.ac.id>
2024-11-12 10:00:52 +0700

    editing home page

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,6 +3,7 @@ import "../styles/home.css";
 import dog1Image from "../assets/dog1.png"; // Replace with actual path
 import catImage from "../assets/cat.png";
 import dog2Image from "../assets/dog2.png";
+import background from "../assets/bg.png";
 
 const Home = () => {
     return (
@@ -10,17 +11,21 @@ const Home = () => {
             <div className="home__container">
                 <div className="home__content">
                     <h1 className="home__title">
-                        Pawsitively <span>Purrfect Care</span>
+                        Pawsitively 
+                    </h1>
+                    <h1 className="home__title2">
+                        Purrfect Care 
                     </h1>
                     <p className="home__description">
-                        We provide quality pet care and personalized treatment plans for your pet.
+                        Find a Friend, Give a Home, Change a Life
                     </p>
                     <button className="home__button">Let's Find Your Pet!</button>
                 </div>
                 <div className="home__images">
-                    <img src={dog1Image} alt="Dog" className="home__image large" />
+                    <img src={background} alt="Background" className="home__image background" />
+                    {/* <img src={dog1Image} alt="Dog" className="home__image large" />
                     <img src={dog2Image} alt="Smiling Pet" className="home__image medium" />
-                    <img src={catImage} alt="Cat" className="home__image small" />
+                    <img src={catImage} alt="Cat" className="home__image small" /> */}
                 </div>
                 <div className="circle circle--large"></div>
                 <div className="circle circle--medium"></div>
