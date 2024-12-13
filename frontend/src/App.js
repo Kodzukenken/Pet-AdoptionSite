ea387cf86c211b8d3ff69bb06ed91f59530cbd3a
michellelyandryy <michelle.lyandry@student.sgu.ac.id>
2024-12-13 23:33:17 +0700

    app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
                       {/* <Route path="/about" element={<About />} />
                       <Route path="/adoption-system" element={<Adopt />} />
                       <Route path="/blog" element={<Blog />} /> */}
+                      {/* <Route path="/CatAdoption" element={<CatAdoption />} /> */}
                   </Routes>
               </div>
               <Footer />
